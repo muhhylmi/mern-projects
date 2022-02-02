68b3d3da97cfac226eb3c6b6248b7629b2bb61a7
muhhylmi <hylmi@mail.adepthq.com>
2022-02-02 18:57:23 +0700

    create get and delete product, create chart data from API

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,59 @@
 import Chart from "../../component/chart/Chart";
 import FeaturedInfo from "../../component/featuredInfo/FeaturedInfo";
 import "./home.css";
-import { userData } from "../../dummyData";
 import WidgetSm from "../../component/widgetSm/WidgetSm";
 import WidgetLg from "../../component/widgetLg/WidgetLg";
+import { useEffect, useMemo, useState } from "react";
+import { userRequest } from "../../requestMethods";
 
 export default function Home() {
+  const [userStats, setUserStats] = useState([]);
+  const MONTHS = useMemo(
+    () => [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const getStats = async () => {
+      try {
+        const res = await userRequest.get("/users/stats");
+        res.data.map((item) =>
+          setUserStats((prev) => [
+            ...prev,
+            { name: MONTHS[item._id - 1], "Active User": item.total },
+          ])
+        );
+      } catch {}
+    };
+    getStats();
+  }, [MONTHS]);
+
   return (
     <div className="home">
       <FeaturedInfo />
-      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <Chart
+        data={userStats}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
       <div className="homeWidgets">
-        <WidgetSm/>
-        <WidgetLg/>
-      </div>  
+        <WidgetSm />
+        <WidgetLg />
+      </div>
     </div>
   );
 }
--- src/redux/apiCalls.js
+++ src/redux/apiCalls.js
@@ -1,5 +1,6 @@
 import { loginFailure, loginStart, loginSuccess } from "./userRedux";
 import { publicRequest } from "../requestMethods";
+import { deleteProductFailure, deleteProductStart, deleteProductSuccess, getProductFailure, getProductStart, getProductSuccess } from "./productRedux";
 
 export const login = async (dispatch, user) => {
     dispatch(loginStart());
@@ -8,5 +9,25 @@ export const login = async (dispatch, user) => {
         dispatch(loginSuccess(res.data));
     } catch (err) {
         dispatch(loginFailure());
+    }
+};
+
+export const getProducts = async (dispatch) => {
+    dispatch(getProductStart());
+    try {
+        const res = await publicRequest.get("/products");
+        dispatch(getProductSuccess(res.data));
+    } catch (err) {
+        dispatch(getProductFailure());
+    }
+};
+
+export const deleteProduct = async (id, dispatch) => {
+    dispatch(deleteProductStart());
+    try {
+        // const res = await userRequest.delete(`/products/${id}`);
+        dispatch(deleteProductSuccess(id));
+    } catch (err) {
+        dispatch(deleteProductFailure());
     }
 };
